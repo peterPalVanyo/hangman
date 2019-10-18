827d37a9c2f631796cb0cc493328a017f51feef2
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-10-18 16:06:08 +0200

    basic guess logic and key press handler

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 const Hangman = function(word, remaining) {
     this.word = word.toLowerCase().split('')
     this.remaining = remaining
-    this.guessedLetters = ['k']
+    this.guessedLetters = []
 }
 
 
@@ -13,7 +13,27 @@ Hangman.prototype.getPuzzle = function() {
     return puzzle
 }
 
-const hangman = new Hangman('akarok barackot', 3)
+Hangman.prototype.makeGuess = function(letter) {
+    const isNew = !this.guessedLetters.includes(letter)
+    const isNotScore = !this.word.includes(letter) 
+    const isCharacter = typeof letter === 'string' && letter.length === 1
+    if(isNew && isCharacter && isNotScore) {
+        this.guessedLetters.push(letter)
+        this.remaining--}
+    if(isNew && isCharacter && !isNotScore) {
+        this.guessedLetters.push(letter)
+    }
+    if(!isNew || !isCharacter) console.log('next time...')
+}
 
-console.log(hangman.word)
+const hangman = new Hangman('akarok barackot', 3)
 console.log(hangman.getPuzzle())
+
+window.addEventListener('keypress', function(e) {
+    const letter = String.fromCharCode(e.charCode)
+    hangman.makeGuess(letter)
+    console.log(hangman.getPuzzle())
+    console.log(hangman.remaining + " " + hangman.guessedLetters)
+})
+
+
