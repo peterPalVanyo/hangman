4edd45c0dfc7688bd2b33c869a58c69d7869ae32
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-10-21 14:44:26 +0200

    play the game with promise

--- app.js
+++ app.js
@@ -14,35 +14,17 @@ window.addEventListener('keypress', function(e) {
     scoreEl.textContent = hangman.statusMessage
 })
 
-getWords((error, words) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log(words)
-    }
+getWords(3).then((words) => {
+    console.log(words)
+}, (err) => {
+    console.log(`error: ${err}`)
 })
 
-/* const request = new XMLHttpRequest()
-request.addEventListener('readystatechange', (e) => {
-    if(e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)    
-    } else if(e.target.readyState === 4) {
-        console.log('sorry there is some problem...') 
-    }
-})
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send() */
 
 
-/* const requestC = new XMLHttpRequest()
-requestC.addEventListener('readystatechange', (e) => {
-    const countries = JSON.parse(e.target.responseText)
-    countries.forEach(element => {
-        if(element.alpha2Code === 'HU') {
-            console.log(element.name)
-        }
-    });
+
+getCountry('HU').then((name) => {
+    console.log(name)
+}, (err) => {
+    console.log(err)
 })
-requestC.open('GET', 'https://restcountries.eu/rest/v2/all')
-requestC.send() */
--- requests.js
+++ requests.js
@@ -1,15 +1,29 @@
-const getWords = (callback) => {
-
-
+const getWords = (count) => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest()
     request.addEventListener('readystatechange', (e) => {
         if(e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.responseText)
-            callback(undefined, data.puzzle)
+            resolve(data.puzzle)
         } else if(e.target.readyState === 4) {
-            callback('sorry there is some problem', undefined)
+            reject('sorry there is some problem')
             }
     })
-    request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+    request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${count}`)
     request.send()
-}
+})
+
+const getCountry = (code) => new Promise((resolve, reject) => {
+    const requestC = new XMLHttpRequest()
+    requestC.addEventListener('readystatechange', (e) => {
+        if(e.target.readyState === 4 && e.target.status === 200) {
+            const countries = JSON.parse(e.target.responseText)
+            const c = countries.find((e) => {return e.alpha2Code === code})
+            resolve(c.name)
+        } else if(e.target.readyState === 4) {
+            reject('country problem')
+        }
+    })
+        requestC.open('GET', 'https://restcountries.eu/rest/v2/all')
+        requestC.send()
+})
+
