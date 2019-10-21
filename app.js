6e3e9cf9766d809ae9d0a4385f9efb49cb33ac42
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-10-21 10:30:46 +0200

    http reqest for words with callback

--- app.js
+++ app.js
@@ -14,3 +14,35 @@ window.addEventListener('keypress', function(e) {
     scoreEl.textContent = hangman.statusMessage
 })
 
+getWords((error, words) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log(words)
+    }
+})
+
+/* const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', (e) => {
+    if(e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)    
+    } else if(e.target.readyState === 4) {
+        console.log('sorry there is some problem...') 
+    }
+})
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+request.send() */
+
+
+/* const requestC = new XMLHttpRequest()
+requestC.addEventListener('readystatechange', (e) => {
+    const countries = JSON.parse(e.target.responseText)
+    countries.forEach(element => {
+        if(element.alpha2Code === 'HU') {
+            console.log(element.name)
+        }
+    });
+})
+requestC.open('GET', 'https://restcountries.eu/rest/v2/all')
+requestC.send() */
